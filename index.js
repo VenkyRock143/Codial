d0e5db5d80eb3c5d3165660d3b435d940f43ea6f
VenkyRock143 <venky.balusani1@gmail.com>
2023-03-28 09:14:49 +0530

    remote

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const homeController = require('../Codial/controller/home_controller')
 
+router.get('./',homeController.home)
+
+app.use('/',require('./router'))
 app.listen(port,function(err){
     if(err){
         console.log(`error occured:${error}`)
