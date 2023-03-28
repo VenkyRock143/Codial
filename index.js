95ce415dccf195b03ddb2d56fa35cf1f6957ea5a
VenkyRock143 <venky.balusani1@gmail.com>
2023-03-28 12:33:13 +0530

    "comit"

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const homeController = require('../Codial/controller/home_controller')
 
 app.use('/',require('./router'))
 
-
+app.set('view engine', 'ejs')
+app.set('views','./views')
 
 
 app.listen(port,function(err){
