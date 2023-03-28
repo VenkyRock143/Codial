3cf7dac004690bc9821506f1bd2b333153acdb6e
VenkyRock143 <venky.balusani1@gmail.com>
2023-03-28 12:31:18 +0530

    ejs view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 const homeController = require('../Codial/controller/home_controller')
 
 app.use('/',require('./router'))
+
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`error occured:${error}`)
