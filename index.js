a9e1a7dfb4d3571acb602bce8a70a95e281b2aba
VenkyRock143 <venky.balusani1@gmail.com>
2023-03-30 18:23:50 +0530

    added cookies

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose')
+const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
 
 app.use(expressLayouts);
 app.use(express.static('./assets'));
