97aa9f63142ae0af2a5fe9517adee56bfb7ece40
VenkyRock143 <venky.balusani1@gmail.com>
2023-03-30 16:31:18 +0530

    step forward

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -3,3 +3,25 @@ module.exports.profile = function(req ,res){
         title:'Profile'
     })
 }
+//Rendering the sign Up page
+module.exports.signUp  = function(req ,res){
+     res.render('user_sign_up',{
+        title: "Codial|sign Up"
+    })
+}
+//Rendering the sign In page
+module.exports.signIn  = function(req ,res){
+    res.render('user_sign_in',{
+       title: "Codial|sign In"
+   })
+}
+
+//get the sign up data
+module.exports.create = function(req ,res){
+
+}
+
+//get the sign In data
+module.exports.create_session = function(req,res){
+
+}
--- router/user.js
+++ router/user.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router();
 const userController = require('../controller/user_controller')
 
-router.get('/profile',userController.profile)
+router.get('/profile',userController.profile);
+router.get('/sign_up',userController.signUp);
+router.get('/sign_in',userController.signIn)
+
 
 module.exports = router;
