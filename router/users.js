efb3506a920c20e7aba4ee6a4dff7b7691857db7
VenkyRock143 <venky.balusani1@gmail.com>
2023-04-02 12:00:40 +0530

    changed

--- router/users.js
+++ router/users.js
@@ -7,7 +7,6 @@ router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
 
 router.post('/create',userController.create);
-router.post('/create-session',userController.create_session)
 
 
 module.exports = router;
