da376fcad98cb30f3170957d183a7d87b8744591
VenkyRock143 <venky.balusani1@gmail.com>
2023-04-02 12:45:57 +0530

    yes

--- router/users.js
+++ router/users.js
@@ -6,7 +6,10 @@ router.get('/profile',userController.profile);
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
 
-router.post('/create',userController.create);
+
+router.post('/create',userController.create); 
+router.post('/create-session',userController.create_session) 
+router.get('/create-session',userController.profile);
 
 
 module.exports = router;
