617988ef56208772af0783bcac8b3d142a9d414e
VenkyRock143 <venky.balusani1@gmail.com>
2023-04-18 01:26:19 +0530

    sending mail in html Template

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -4,12 +4,13 @@ const nodeMailer = require('../config/nodemailer');
 
 exports.newComment = (comment) =>{
     console.log(comment);
+    let htmlString = nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from:'venky.balusani6@gmail.com',
         to: comment.user.email,
         subject: "Yup! this is done",
-        html:'<h1>recieved my first mail in project</h1>'
+        html:htmlString
     },(err,info)=> {
         if(err){
             console.log('Error in sending mail',err);
