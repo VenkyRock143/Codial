f3be0a766a358eb40f2fe1d1c33f2cdea447a566
VenkyRock143 <venky.balusani1@gmail.com>
2024-06-13 02:10:40 +0530

    f

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const path               =       require('path')
 
 const redis = require('redis');
 const redisClient = redis.createClient({
-    url: 'redis://127.0.0.1:6379',
+    url: 'redis://red-cpkps0fsc6pc73f27deg:6379',
     socket: {
         connectTimeout: 50000, // increase connection timeout if needed
     },
