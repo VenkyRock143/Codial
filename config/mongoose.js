b75f2c76daa81ead0500140cc1d6c2934f8edac5
VenkyRock143 <venky.balusani1@gmail.com>
2024-06-13 03:27:57 +0530

    h

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,12 +6,31 @@ mongoose.connect(env.db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000, // 30 seconds
-    socketTimeoutMS: 45000, // 45 seconds
-}).then(() => {
-    console.log('MongoDB connected successfully');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
 });
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through app termination');
+    process.exit(0);
+  });
+});
+// }).then(() => {
+//     console.log('MongoDB connected successfully');
+// }).catch((err) => {
+//     console.error('Error connecting to MongoDB:', err.message);
+// });
 
 const db = mongoose.connection;
 
