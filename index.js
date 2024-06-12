b228f260003064c31db308f55d5225ac600c67eb
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2024-06-12 18:54:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,26 @@ const sassMiddleware     =       require('sass-middleware');
 const flash              =       require('connect-flash');
 const customMware        =       require('./config/middleware');
 const path               =       require('path')
-const { redisClient, redisStore } = require('./config/redis');
 
+import { createClient } from 'redis';
+
+(async () => {
+  // Connect to your internal Redis instance using the REDIS_URL environment variable
+  // The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
+  const client = createClient({
+      url: process.env.REDIS_URL
+  });
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  // Send and retrieve some values
+  await client.set('key', 'node redis');
+  const value = await client.get('key');
+
+  console.log("found value: ", value)
+})();
 
 //setup the chat serverto be used with socket.io
 const chatServer = require('http').createServer(app);
@@ -70,20 +88,20 @@ app.use(session({
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
-    store: redisStore
-    // store: new MongoStore(
-    //     {
-    //         mongooseConnection:db,
-    //         autoremove:'disabled'
-    //     }
-    //     ,
-    //     {
-    //         function(err){
-    //             console.log(err || 'connect-mongodb setup ok')
-    //         }
-    //     }
+    // store: redisStore
+    store: new MongoStore(
+        {
+            mongooseConnection:db,
+            autoremove:'disabled'
+        }
+        ,
+        {
+            function(err){
+                console.log(err || 'connect-mongodb setup ok')
+            }
+        }
         
-    //     )
+        )
 }));
 
 app.use(passport.initialize());
