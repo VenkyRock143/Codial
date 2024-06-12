5c315879d67dc04f931137ceb273c4d5216193e1
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2024-06-12 16:08:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,64 +14,74 @@ const passportLocal      =       require('./config/passport-local-strategy');
 const passportJWT        =       require('./config/passport-jwt-strategy');
 const passportGoogle     =       require('./config/passport-google-oauth-strategy')
 const MongoStore         =       require('connect-mongo')(session);
-// const sassMiddleware  =       require('node-sass-middleware');
-const sass               =       require('sass');
-const path               =       require('path');
-const fs                 =       require('fs');
+const sassMiddleware     =       require('node-sass-middleware');
 const flash              =       require('connect-flash');
 const customMware        =       require('./config/middleware');
+
+
 
 //setup the chat serverto be used with socket.io
 const chatServer = require('http').createServer(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port 5000')
+const path               =       require('path')
 
-if (env.name === 'development') {
-    // Compile SCSS to CSS
-    const result = sass.renderSync({
-        file: path.join(__dirname, 'assets', 'scss', 'style.scss'),
-        outFile: path.join(__dirname, 'assets', 'css', 'style.css'),
-        outputStyle: 'extended'
-    });
 
-    // Write the result to a file
-    fs.writeFileSync(path.join(__dirname, 'assets', 'css', 'style.css'), result.css);
+if (env.name == 'development'){
+    app.use(sassMiddleware({
+        src: path.join(__dirname,env.CODEIAL_ASSET_PATH, 'scss'),
+        dest: path.join(__dirname,env.CODEIAL_ASSET_PATH, 'css'),
+        debug: true,
+        outputStyle: 'extended',
+        prefix: '/css'
+    }));
 }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(env.asset_path));
+app.use(express.urlencoded());
 
-// Make the uploads path available to browser
+app.use(cookieParser());
+
+app.use(express.static(env.asset_path));
+//make the uploads path available to browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts);
-// Extract style and scripts from subpages into the layout
+// extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// Set up the view engine
+
+
+
+// set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// MongoStore is used to store the session cookie in db
+//mongostore is used to store the session cookie in db
 app.use(session({
     name: 'codeial',
-    // TODO: change the secret before deployment in production mode
+    // TODO change the secret before deployment in production mode
     secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
-    store: new MongoStore({
-        mongooseConnection: db,
-        autoremove: 'disabled'
-    }, function (err) {
-        console.log(err || 'connect-mongodb setup ok');
-    })
+    store: new MongoStore(
+        {
+            mongooseConnection:db,
+            autoremove:'disabled'
+        }
+        ,
+        {
+            function(err){
+                console.log(err || 'connect-mongodb setup ok')
+            }
+        }
+        
+        )
 }));
 
 app.use(passport.initialize());
@@ -81,13 +91,14 @@ app.use(passport.setAuthenticatedUser);
 
 app.use(flash());
 app.use(customMware.setFlash);
-
-// Use express router
+// use express router
 app.use('/', require('./routes'));
 
-app.listen(port, function (err) {
-    if (err) {
+
+app.listen(port, function(err){
+    if (err){
         console.log(`Error in running the server: ${err}`);
     }
+
     console.log(`Server is running on port: ${port}`);
 });
