3af6f07dccd09d0cdce5411905c1019ddd79d17e
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2024-06-12 19:45:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,15 @@ const path               =       require('path')
 // const { redisClient, redisStore } = require('./config/redis');
 
 const { createClient } = require('redis');
-const RedisStore = require('connect-redis');
+// const connectRedis = require('connect-redis');
 
 // Log the Redis URL for debugging
 console.log('Connecting to Redis at:', process.env.REDIS_URL);
+
+// const RedisStore = connectRedis(session);
+const connectRedis = require('connect-redis');
+const RedisStore = connectRedis(session);
+
 
 (async () => {
   try {
@@ -61,7 +66,7 @@ console.log('Connecting to Redis at:', process.env.REDIS_URL);
     app.use(cookieParser());
 
     app.use(express.static(env.asset_path));
-    // Make the uploads path available to browser
+    // Make the uploads path available to the browser
     app.use('/uploads', express.static(__dirname + '/uploads'));
     app.use(logger(env.morgan.mode, env.morgan.options));
 
@@ -108,86 +113,3 @@ console.log('Connecting to Redis at:', process.env.REDIS_URL);
     console.error('Error connecting to Redis', err);
   }
 })();
-//setup the chat serverto be used with socket.io
-// const chatServer = require('http').createServer(app);
-// const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-// chatServer.listen(5000);
-// console.log('chat server is listening on port 5000')
-
-// if (env.name == 'development'){
-//     app.use(sassMiddleware({
-//         src: path.join(__dirname, env.asset_path, 'scss'),
-//         dest: path.join(__dirname, env.asset_path, 'css'),
-//         debug: true,
-//         outputStyle: 'extended',
-//         prefix: '/css'
-//     }));
-// }
-
-
-
-// app.use(express.urlencoded());
-
-// app.use(cookieParser());
-
-// app.use(express.static(env.asset_path));
-// //make the uploads path available to browser
-// app.use('/uploads', express.static(__dirname + '/uploads'));
-// app.use(logger(env.morgan.mode, env.morgan.options));
-
-// app.use(expressLayouts);
-// // extract style and scripts from sub pages into the layout
-// app.set('layout extractStyles', true);
-// app.set('layout extractScripts', true);
-
-
-
-
-// // set up the view engine
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
-
-// //mongostore is used to store the session cookie in db
-// app.use(session({
-//     name: 'codeial',
-//     // TODO change the secret before deployment in production mode
-//     secret: env.session_cookie_key,
-//     saveUninitialized: false,
-//     resave: false,
-//     cookie: {
-//         maxAge: (1000 * 60 * 100)
-//     },
-//     // store: redisStore
-//     store: new MongoStore(
-//         {
-//             mongooseConnection:db,
-//             autoremove:'disabled'
-//         }
-//         ,
-//         {
-//             function(err){
-//                 console.log(err || 'connect-mongodb setup ok')
-//             }
-//         }
-        
-//         )
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.use(passport.setAuthenticatedUser);
-
-// app.use(flash());
-// app.use(customMware.setFlash);
-// // use express router
-// app.use('/', require('./routes'));
-
-
-// app.listen(port, function(err){
-//     if (err){
-//         console.log(`Error in running the server: ${err}`);
-//     }
-
-//     console.log(`Server is running on port: ${port}`);
-// });
