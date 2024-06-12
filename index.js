70716fa99119ea7e80d7870cacc5d0d190f55291
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2024-06-12 19:01:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,24 +19,27 @@ const flash              =       require('connect-flash');
 const customMware        =       require('./config/middleware');
 const path               =       require('path')
 
-import { createClient } from 'redis';
+const { createClient } = require('redis');
 
 (async () => {
-  // Connect to your internal Redis instance using the REDIS_URL environment variable
-  // The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
-  const client = createClient({
+  try {
+    // Connect to your internal Redis instance using the REDIS_URL environment variable
+    const client = createClient({
       url: process.env.REDIS_URL
-  });
+    });
 
-  client.on('error', (err) => console.log('Redis Client Error', err));
+    client.on('error', (err) => console.log('Redis Client Error', err));
 
-  await client.connect();
+    await client.connect();
 
-  // Send and retrieve some values
-  await client.set('key', 'node redis');
-  const value = await client.get('key');
+    // Send and retrieve some values
+    await client.set('key', 'node redis');
+    const value = await client.get('key');
 
-  console.log("found value: ", value)
+    console.log('found value: ', value);
+  } catch (err) {
+    console.error('Error connecting to Redis', err);
+  }
 })();
 
 //setup the chat serverto be used with socket.io
